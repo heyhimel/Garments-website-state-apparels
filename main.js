d9833451411bbaf8a70001a286ee2ea7b624d1ca
Kazi Tanvir Ahmed <kazitanvirahmed571@gmail.com>
2024-07-14 02:50:47 +0600

    caurosel slider added to subpage4

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+// hambarger
 (function() {
 
     'use strict';
@@ -23,3 +25,38 @@
       });
     
     })();
+
+    // working procedure page, dynamic adjust main section postion according to header
+
+    function adjustContentPosition() {
+      const header = document.getElementById('header');
+      const content = document.getElementById('main');
+      const headerHeight = header.offsetHeight;
+      content.style.marginTop = headerHeight + 'px';
+  }
+
+  window.addEventListener('resize', adjustContentPosition);
+  window.addEventListener('load', adjustContentPosition);
+
+  //working procedure page, slider section
+
+  let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  
+  slides[slideIndex-1].style.display = "block";  
+  
+}

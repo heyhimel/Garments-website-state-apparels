1ce8eb7f68397d27b3ecda818a98e3e28e841d79
Kazi Tanvir Ahmed <kazitanvirahmed571@gmail.com>
2024-07-14 17:04:40 +0600

    subpage 4 and 5

--- main.js
+++ main.js
@@ -50,7 +50,6 @@ function plusSlides(n) {
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
   if (n > slides.length) {slideIndex = 1}    
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -59,4 +58,62 @@ function showSlides(n) {
   
   slides[slideIndex-1].style.display = "block";  
   
+}
+
+// what we do page slider section
+// slider1
+
+let slideIndex1 = 1;
+showSlides1(slideIndex1);
+
+function plusSlides1(n) {
+  showSlides1(slideIndex1 += n);
+}
+
+function showSlides1(n) {
+  let i;
+  let slides1 = document.getElementsByClassName("mySlides1");
+  if (n > slides1.length) {slideIndex1 = 1}    
+  if (n < 1) {slideIndex1 = slides1.length}
+  for (i = 0; i < slides1.length; i++) {
+    slides1[i].style.display = "none";  
+  }
+  
+  slides1[slideIndex1-1].style.display = "block";  
+  
+}
+
+// slider2
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function plusSlides2(n) {
+  showSlides2(slideIndex2 += n);
+}
+
+function showSlides2(n) {
+  let i;
+  let slides2 = document.getElementsByClassName("mySlides2");
+  if (n > slides2.length) {slideIndex2 = 1}    
+  if (n < 1) {slideIndex2 = slides2.length}
+  for (i = 0; i < slides2.length; i++) {
+    slides2[i].style.display = "none";  
+  }
+  
+  slides2[slideIndex2-1].style.display = "block";  
+  
+}
+
+// collapse, subpage5
+function minusicon(n){
+  let btn = document.getElementById("btn"+n);
+  if(btn.innerHTML=="-")
+  {
+    btn.innerHTML = "+";
+  }
+  else
+  {
+    btn.innerHTML = "-";
+  }
 }
